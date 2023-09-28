3528012e41dd1884d5cc3c16a423e7ed435e06e8
Matt Richardson <matt.richardson@msrconsults.com>
2023-09-28 14:52:45 -0600

    Functional code.  Now need to get the timing better

--- i2s_dma_tdm_record_playback.c
+++ i2s_dma_tdm_record_playback.c
@@ -97,6 +97,13 @@ typedef struct event_s {
     uint64_t evt_times[5];
 } event_t;
 
+typedef enum evt_type_e
+{
+    RCV_INTERRUPT = 0,
+    START_TXMIT = 1,
+    RCV_TXMIT = 2
+} evt_type_t;
+
 static uint64_t g_tick_per_us = 0;
 
 static uint64_t ostime_get_us();
@@ -106,13 +113,13 @@ static event_t current_events = {.idx = 0, .evt_times = {0}};
 /*******************************************************************************
  * Code
  ******************************************************************************/
-static void update_evt_times(event_t *events);
+static void update_evt_times(evt_type_t evt_type, event_t *events);
 
 static void generate_wave()
 {
 	// This is generating a shit wave but it works for the moment
 	// Volume is 1/4 max
-	int32_t max_vol = 88080384;
+	int32_t max_vol = 16777216;
 
 	PRINTF("Generating wave now...\r\n");
 
@@ -120,12 +127,11 @@ static void generate_wave()
 	{
 		wave[i] = (int32_t)(max_vol * sin(2*M_PI /48*i));
 	}
-
 }
 
 void APP_GPIO_INTA_IRQHandler(void)
 {
-    update_evt_times(&current_events);
+    update_evt_times(RCV_INTERRUPT, &current_events);
     /* clear the interrupt status */
     GPIO_PinClearInterruptFlag(SW2_GPIO, SW2_PORT, SW2_PIN, 0);
     /* Change state of switch. */
@@ -165,14 +171,17 @@ static uint64_t ostime_get_us()
 
 }
 
-static void update_evt_times(event_t *events)
+static void update_evt_times(evt_type_t evt_type, event_t *events)
 {
-    events->evt_times[events->idx++] = ostime_get_us();
+    uint64_t c_time = ostime_get_us();
+    events->evt_times[evt_type] = c_time;
+    PRINTF("%d: %d\r\n", evt_type, c_time);
+}
 
-    if (events->idx >= 5)
-    {
-        events->idx = 0;
-    }
+static void print_evt_data(event_t events)
+{
+    PRINTF("EVENT\tTIME\r\nINT_RCV\t%d\r\nTXMIT_START\t%d\r\nTXMIT_RCV\t%d\r\n",
+        events.evt_times[RCV_INTERRUPT], events.evt_times[START_TXMIT], events.evt_times[RCV_TXMIT]);
 }
 static void rcv_i2s_data(i2s_transfer_t *rcv_transfer)
 {
@@ -185,25 +194,29 @@ static void rcv_i2s_data(i2s_transfer_t *rcv_transfer)
     static bool transmitting = false;
     static bool trans_rcv = false;
 
-    if (!trans_rcv && transmitting && new_data[8] == wave[1] && new_data[16] == wave[2]) {
-        update_evt_times(&current_events);
+    if (!trans_rcv && transmitting && new_data[0] > 0) {
+        update_evt_times(RCV_TXMIT, &current_events);
+        PRINTF("Receiving transmission\r\n");
+        print_evt_data(current_events);
         trans_rcv = true;
     }
 
+    uint32_t num_elements = (SAMPLE_SIZE_MS * SAMPLE_PER_MS *  NUM_CHANNELS);
 
 	// Only intercept the signal if the interrupt is active...
 	if (g_interruptEnabled){
         if (!transmitting)
         {
-            update_evt_times(&current_events);
+            update_evt_times(START_TXMIT, &current_events);
             transmitting = true;
         }
 
-		uint32_t num_elements = (SAMPLE_SIZE_MS * SAMPLE_PER_MS *  NUM_CHANNELS);
-
-
 		for (uint32_t i = 0, wave_pos = 0; i < num_elements; wave_pos++)
 		{
+            // Copy incoming data on 0 and 1 into 2 and 3
+			new_data[i+2] = new_data[i];
+			new_data[i+3] = new_data[i+1];
+
 			new_data[i++] = wave[wave_pos % 48];
 			new_data[i++] = wave[wave_pos % 48];
 			i+=6;
@@ -211,6 +224,16 @@ static void rcv_i2s_data(i2s_transfer_t *rcv_transfer)
 	}
     else
     {
+		for (uint32_t i = 0, wave_pos = 0; i < num_elements; wave_pos++)
+		{
+            // Copy incoming data on 0 and 1 into 2 and 3
+			new_data[i+2] = new_data[i];
+			new_data[i+3] = new_data[i+1];
+
+			new_data[i++] = 0;
+			new_data[i++] = 0;
+			i+=6;
+		}
         trans_rcv = transmitting = false;
 
     }
@@ -231,7 +254,6 @@ static void rcv_i2s_data(i2s_transfer_t *rcv_transfer)
 }
 static void i2s_rx_Callback(I2S_Type *base, i2s_dma_handle_t *handle, status_t completionStatus, void *userData)
 {
-
     emptyBlock--;
 }
 
@@ -387,6 +409,8 @@ static void DEMO_InitCodec(void)
         PRINTF("CODEC_Init failed!\r\n");
         assert(false);
     }
+    CODEC_SetVolume(&codecHandle, 0, 100);
+    CODEC_SetVolume(&codecHandle, 1, 100);
 
     PRINTF("\r\nCodec Init Done.\r\n");
 }
